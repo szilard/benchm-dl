d2d276b4118e1aab09bd47d4f5c0b173fda36d62
szilard <szilard@users.noreply.github.com>
2016-11-01 13:00:39 -0700

    keras w init

--- cnn-mnist/1x-keras-xtra.py
+++ cnn-mnist/1x-keras-xtra.py
@@ -9,6 +9,7 @@ from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D, Dense, Flatten, Dropout
 from keras.optimizers import SGD
 from keras.callbacks import EarlyStopping
+from keras import initializations
 from keras.utils import np_utils
 from keras import backend as K
 import numpy as np
@@ -42,17 +43,21 @@ Y_train = np_utils.to_categorical(y_train, 10)
 Y_test = np_utils.to_categorical(y_test, 10)
 
 
+def my_init(shape, name=None):
+    return initializations.normal(shape, scale=0.1, name=name)
+
 model = Sequential()
-model.add(Convolution2D(32, 4, 4, input_shape = input_shape, activation = 'relu'))
+model.add(Convolution2D(32, 4, 4, input_shape = input_shape, activation = 'relu', init = my_init))
 model.add(MaxPooling2D(pool_size = (2,2)))
-model.add(Convolution2D(16, 3, 3, activation = 'relu'))
+model.add(Convolution2D(16, 3, 3, activation = 'relu', init = my_init))
 model.add(MaxPooling2D(pool_size = (2,2)))
 model.add(Dropout(0.2))
 
 model.add(Flatten())
-model.add(Dense(128, activation = 'relu'))
-model.add(Dense(64, activation = 'relu'))
-model.add(Dense(10, activation = 'softmax'))
+model.add(Dense(128, activation = 'relu', init = my_init))
+model.add(Dense(64, activation = 'relu', init = my_init))
+model.add(Dense(10, activation = 'softmax', init = my_init))
+
 
 sgd = SGD(lr=0.05, decay=1e-5, momentum=0.9)
 model.compile(loss = 'categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
