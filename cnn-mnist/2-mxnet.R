eb9c66d7d56c02c759c3b839573ac512ae123d7a
szilard <szilard@users.noreply.github.com>
2016-11-04 21:04:32 -0700

    mxnet

--- cnn-mnist/2-mxnet.R
+++ cnn-mnist/2-mxnet.R
@@ -49,7 +49,8 @@ tic <- proc.time()
 model <- mx.model.FeedForward.create(model_spec, X = train.array, y = train.y,
                initializer = mx.init.normal(0.1),
                eval.metric = mx.metric.accuracy,
-               optimizer = mxnet:::mx.opt.sgd(learning.rate = 0.05, momentum = 0.9, wd = 1e-5),
+               ##optimizer = mxnet:::mx.opt.sgd(learning.rate = 0.05, momentum = 0.9, wd = 1e-5),  ## bug?
+               learning.rate = 0.05, momentum = 0.9, wd = 1e-5,
                ctx = device.gpu, 
                num.round = 10, array.batch.size = 128,
                epoch.end.callback = mx.callback.log.train.metric(100))
